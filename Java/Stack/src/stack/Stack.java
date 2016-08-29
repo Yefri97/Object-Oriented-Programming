c91b1566ebaf3eb53eac48ff4de2f455c63eef49
Yeferson Gaitan <yefri.gaitan97@gmail.com>
2016-08-28 21:19:58 -0500

    Created Stack in Java

--- Java/Stack/src/stack/Stack.java
+++ Java/Stack/src/stack/Stack.java
@@ -7,37 +7,78 @@ package stack;
 
 /**
  *
- * @author Yefri97
+ * @author yefer
  */
 public class Stack {
 
-    private Node top;
+    private Node first;
     private int size;
     
     private class Node {
-        int val;
-        Node next;
-        public Node(int _val, Node _next) {
-            this.val = _val;
-            this.next = _next;
+        private final int val;
+        private final Node next;
+        public Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
         }
     }
     
-    public Stack() {
-        this.top = null;
-        this.size = 0;
+    public int size() {
+        return size;
+    }
+    
+    public boolean isEmpty() {
+        return first == null;
     }
     
     public void push(int x) {
-        Node n = new Node(x, );
-        if (top == null) {
-            Node n = new 
-        }
+        first = new Node(x, first);
+        size++;
     }
     
+    public int top() {
+        if (isEmpty()) return -1;
+        return first.val;
+    }
+    
+    public int pop() {
+        if (isEmpty()) return 0;
+        first = first.next;
+        size--;
+        return 1;
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola");
+        Stack s = new Stack();
+        
+        System.out.println("The stack is empty? -> " + s.isEmpty());
+        int x = 4;
+        s.push(x);
+        System.out.println("Push -> " + x);
+        System.out.println("The stack is empty? -> " + s.isEmpty());
+        x = 2;
+        s.push(x);
+        System.out.println("Push -> " + x);
+        x = 1;
+        s.push(x);
+        System.out.println("Push -> " + x);
+        System.out.println("Top -> " + s.top());
+        System.out.println("Size -> " + s.size());
+        s.pop();
+        System.out.println("Pop");
+        System.out.println("Top -> " + s.top());
+        System.out.println("Size -> " + s.size());
+        s.pop();
+        System.out.println("Pop");
+        System.out.println("Top -> " + s.top());
+        System.out.println("Size -> " + s.size());
+        s.pop();
+        System.out.println("Pop");
+        System.out.println("The stack is empty? -> " + s.isEmpty());
     }
-    
+
 }
