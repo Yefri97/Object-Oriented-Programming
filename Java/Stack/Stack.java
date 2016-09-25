5af7234b3ca4aa30c4712ff2536c4ed7045c7ab5
Yeferson Gaitan <yefri.gaitan97@gmail.com>
2016-09-25 17:00:21 -0500

    Organized folders Java and Python

--- Java/Stack/Stack.java
+++ Java/Stack/Stack.java
@@ -2,70 +2,70 @@ package stack;
 
 public class Stack {
 
-    private Node first;
-    private int size;
-    
-    private class Node {
-        private final int val;
-        private final Node next;
-        public Node(int val, Node next) {
-            this.val = val;
-            this.next = next;
-        }
+  private Node first;
+  private int size;
+  
+  private class Node {
+    private final int val;
+    private final Node next;
+    public Node(int val, Node next) {
+      this.val = val;
+      this.next = next;
     }
-    
-    public int size() {
-        return size;
-    }
-    
-    public boolean isEmpty() {
-        return first == null;
-    }
-    
-    public void push(int x) {
-        first = new Node(x, first);
-        size++;
-    }
-    
-    public int top() {
-        if (isEmpty()) return -1;
-        return first.val;
-    }
-    
-    public int pop() {
-        if (isEmpty()) return 0;
-        first = first.next;
-        size--;
-        return 1;
-    }
+  }
+  
+  public int size() {
+    return size;
+  }
+  
+  public boolean isEmpty() {
+    return first == null;
+  }
+  
+  public void push(int x) {
+    first = new Node(x, first);
+    size++;
+  }
+  
+  public int top() {
+    if (isEmpty()) return -1;
+    return first.val;
+  }
+  
+  public int pop() {
+    if (isEmpty()) return 0;
+    first = first.next;
+    size--;
+    return 1;
+  }
 
-    public static void main(String[] args) {
-        Stack s = new Stack();
-        
-        System.out.println("The stack is empty? -> " + s.isEmpty());
-        int x = 4;
-        s.push(x);
-        System.out.println("Push -> " + x);
-        System.out.println("The stack is empty? -> " + s.isEmpty());
-        x = 2;
-        s.push(x);
-        System.out.println("Push -> " + x);
-        x = 1;
-        s.push(x);
-        System.out.println("Push -> " + x);
-        System.out.println("Top -> " + s.top());
-        System.out.println("Size -> " + s.size());
-        s.pop();
-        System.out.println("Pop");
-        System.out.println("Top -> " + s.top());
-        System.out.println("Size -> " + s.size());
-        s.pop();
-        System.out.println("Pop");
-        System.out.println("Top -> " + s.top());
-        System.out.println("Size -> " + s.size());
-        s.pop();
-        System.out.println("Pop");
-        System.out.println("The stack is empty? -> " + s.isEmpty());
-    }
+  public static void main(String[] args) {
+    Stack s = new Stack();
+    
+    System.out.println("The stack is empty? -> " + s.isEmpty());
+    int x = 4;
+    s.push(x);
+    System.out.println("Push -> " + x);
+    System.out.println("The stack is empty? -> " + s.isEmpty());
+    x = 2;
+    s.push(x);
+    System.out.println("Push -> " + x);
+    x = 1;
+    s.push(x);
+    System.out.println("Push -> " + x);
+    System.out.println("Top -> " + s.top());
+    System.out.println("Size -> " + s.size());
+    s.pop();
+    System.out.println("Pop");
+    System.out.println("Top -> " + s.top());
+    System.out.println("Size -> " + s.size());
+    s.pop();
+    System.out.println("Pop");
+    System.out.println("Top -> " + s.top());
+    System.out.println("Size -> " + s.size());
+    s.pop();
+    System.out.println("Pop");
+    System.out.println("The stack is empty? -> " + s.isEmpty());
+  }
 
 }
--- Java/Vector3D/Vector3D.java
+++ Java/Vector3D/Vector3D.java
@@ -2,78 +2,78 @@ package vector3d;
 
 public class Vector3D {
 
-    private float x;
-    private float y;
-    private float z;
-    
-    public Vector3D(float x, float y, float z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
+  private float x;
+  private float y;
+  private float z;
+  
+  public Vector3D(float x, float y, float z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
 
-    public void print() {
-        System.out.println("[" + x + ", " + y + ", " + z + "]");
-    }
-    
-    public static Vector3D add(Vector3D a, Vector3D b) {
-        Vector3D ans = new Vector3D(a.x + b.x, a.y + b.y, a.z + b.z);
-        return ans;
-    }
-    
-    public static Vector3D sub(Vector3D a, Vector3D b) {
-        Vector3D ans = new Vector3D(a.x - b.x, a.y - b.y, a.z - b.z);
-        return ans;
-    }
-    
-    public static Vector3D mult(Vector3D a, float k) {
-        Vector3D ans = new Vector3D(a.x * k, a.y * k, a.z * k);
-        return ans;
-    }
-    
-    public static Vector3D div(Vector3D a, float k) {
-        Vector3D ans = new Vector3D(a.x / k, a.y / k, a.z / k);
-        return ans;
-    }
-    
-    public float mag() {
-        return (float) Math.sqrt(x * x + y * y + z * z);
-    }
-    
-    public void setMag(float k) {
-        Vector3D temp = mult(normalize(this), k);
-        x = temp.x;
-        y = temp.y;
-        z = temp.z;
-    }
-    
-    public static Vector3D normalize(Vector3D v) {
-        return div(v, v.mag());
-    }
-    
-    public static float dist(Vector3D a, Vector3D b) {
-        Vector3D ans = sub(a, b);
-        return ans.mag();
-    }
-    
-    public static float angleBetween(Vector3D a, Vector3D b) {
-        float ans = (float) Math.acos(dot(a, b) / (a.mag() * b.mag()));
-        return (float) (180.0 / Math.PI * ans);
-    }
-    
-    public static float dot(Vector3D a, Vector3D b) {
-        return (a.x * b.x + a.y * b.y + a.z * b.z);
-    }
-    
-    public static void main(String[] args) {
-        Vector3D a = new Vector3D(3, 4, 0), b = new Vector3D(1, -1, 2);
-        a.print();
-        System.out.println("Mag " + a.mag());
-        a.setMag(10);
-        a.print();
-        System.out.println("Mag " + Math.round(a.mag()));
-        Vector3D m = new Vector3D(100, 0, 0), n = new Vector3D(0, 100, 0);
-        System.out.println("Angle between m & n " + angleBetween(m, n));
-    }
-    
+  public void print() {
+    System.out.println("[" + x + ", " + y + ", " + z + "]");
+  }
+  
+  public static Vector3D add(Vector3D a, Vector3D b) {
+    Vector3D ans = new Vector3D(a.x + b.x, a.y + b.y, a.z + b.z);
+    return ans;
+  }
+  
+  public static Vector3D sub(Vector3D a, Vector3D b) {
+    Vector3D ans = new Vector3D(a.x - b.x, a.y - b.y, a.z - b.z);
+    return ans;
+  }
+  
+  public static Vector3D mult(Vector3D a, float k) {
+    Vector3D ans = new Vector3D(a.x * k, a.y * k, a.z * k);
+    return ans;
+  }
+  
+  public static Vector3D div(Vector3D a, float k) {
+    Vector3D ans = new Vector3D(a.x / k, a.y / k, a.z / k);
+    return ans;
+  }
+  
+  public float mag() {
+    return (float) Math.sqrt(x * x + y * y + z * z);
+  }
+  
+  public void setMag(float k) {
+    Vector3D temp = mult(normalize(this), k);
+    x = temp.x;
+    y = temp.y;
+    z = temp.z;
+  }
+  
+  public static Vector3D normalize(Vector3D v) {
+    return div(v, v.mag());
+  }
+  
+  public static float dist(Vector3D a, Vector3D b) {
+    Vector3D ans = sub(a, b);
+    return ans.mag();
+  }
+  
+  public static float angleBetween(Vector3D a, Vector3D b) {
+    float ans = (float) Math.acos(dot(a, b) / (a.mag() * b.mag()));
+    return (float) (180.0 / Math.PI * ans);
+  }
+  
+  public static float dot(Vector3D a, Vector3D b) {
+    return (a.x * b.x + a.y * b.y + a.z * b.z);
+  }
+  
+  public static void main(String[] args) {
+    Vector3D a = new Vector3D(3, 4, 0), b = new Vector3D(1, -1, 2);
+    a.print();
+    System.out.println("Mag " + a.mag());
+    a.setMag(10);
+    a.print();
+    System.out.println("Mag " + Math.round(a.mag()));
+    Vector3D m = new Vector3D(100, 0, 0), n = new Vector3D(0, 100, 0);
+    System.out.println("Angle between m & n " + angleBetween(m, n));
+  }
+  
 }
