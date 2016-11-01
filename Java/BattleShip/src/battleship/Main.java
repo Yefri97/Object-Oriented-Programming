e1fa8559dada3ee06c7a2679c168f9abd84d2059
Yefri97 <yefri.gaitan97@gmail.com>
2016-11-01 11:53:06 -0500

    Init project BattleShip

---
+++ Java/BattleShip/src/battleship/Main.java
@@ -0,0 +1,16 @@
+package battleship;
+
+/**
+ *
+ * @author Yeferson Gaitan Gomez
+ */
+public class Main {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("MAIN");
+    }
+    
+}
