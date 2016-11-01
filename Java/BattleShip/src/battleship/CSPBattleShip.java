a1ebcdef2bd730354e73a8d0d6085c67ac8e061a
Yefri97 <yefri.gaitan97@gmail.com>
2016-11-01 15:58:34 -0500

    Constructor CSP with Variables and Domain

--- Java/BattleShip/src/battleship/CSPBattleShip.java
+++ Java/BattleShip/src/battleship/CSPBattleShip.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package battleship;
+
+import aima.core.search.csp.CSP;
+import aima.core.search.csp.Constraint;
+import aima.core.search.csp.Domain;
+import aima.core.search.csp.Variable;
+import java.util.List;
 
 /**
  *
- * @author web
+ * @author Yeferson Gaitan Gomez
  */
 public class CSPBattleShip {
+    
+  private List<Variable> cells;
+  private final Domain ships;
+  private final Constraint sumRows;
+  private final Constraint sumCols;
+  
+  private final CSP csp;
+  
+  public CSPBattleShip(int sizeBoard, int[] typeShips, int[] row, int[] col) {
+    
+    // Genero todas las variables del tablero -> Xi
+    for (int i = 0; i < sizeBoard * sizeBoard; i++) {
+      Variable Xi = new Variable("X_"+i);
+      cells.add(Xi);
+    }
+    
+    csp = new CSP(cells);
+    
+    // Genero el dominio para las variables
+    // dominio = {0, 1, 2, 3, ... n} -> dominio[i] = id del barco
+    int numShips = typeShips.length;
+    List<Integer> domain = null;
+    for (int i = 0; i <= numShips; i++) domain.add(i);
+    ships = new Domain(domain);
+    
+    // Configuro el dominio para todas la variables del tablero
+    for (int i = 0; i < sizeBoard * sizeBoard; i++) {
+      Variable Xi = new Variable("X_"+i);
+      csp.setDomain(Xi, ships);
+    }
+    
+  }
     
 }
--- Java/BattleShip/src/battleship/Main.java
+++ Java/BattleShip/src/battleship/Main.java
@@ -10,7 +10,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("MAIN");
+      CSPBattleShip csp = new CSPBattleShip();
+      System.out.println("MAIN");
     }
     
 }
